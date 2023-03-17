6122658818e9843fc1daba72ae957f2f66fe74ee
IgorLazarin <igor.lazarin@ufms.br>
2023-03-17 16:14:52 -0400

    Projeto para construção e exemplos de EDs em Java

--- TiposVariaveis/src/TiposVariaveis.java
+++ TiposVariaveis/src/TiposVariaveis.java
@@ -1,5 +1,13 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        byte idade = 18; 
+        short ano = 2021;
+        int cep = 21070333;
+        long cpf = 98765432109L;
+        float pi = 3.14F;
+        double salario = 1275.33;
+
+        //Impedindo a alteração de uma variável para torná-la uma constante:
+        final double VALOR_DE_PI = 3.14;
     }
 }
